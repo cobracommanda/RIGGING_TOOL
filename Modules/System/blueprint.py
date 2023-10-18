6e906051c9a657cfc4ef6a039794e995c583395d
Darrell Robinson <3dfordinner@gmail.com>
2023-10-18 12:20:02 -0400

    locking implementation pt 6

--- Modules/System/blueprint.py
+++ Modules/System/blueprint.py
@@ -460,3 +460,30 @@ class Blueprint:
                 
                 new_joints.append(new_joint)
                 
+                
+                if i < num_rotation_orders:
+                    cmds.setAttr(new_joint+".rotateOrder", int(joint_rotation_orders[i]))
+                
+                if i < num_preferred_angles:
+                    cmds.setAttr(new_joint+".preferredAngleX", int(joint_preferred_angles[i][0]))
+                    cmds.setAttr(new_joint+".preferredAngleY", int(joint_preferred_angles[i][1]))
+                    cmds.setAttr(new_joint+".preferredAngleZ", int(joint_preferred_angles[i][2]))
+                
+                cmds.setAttr(new_joint+".segmentScaleCompensate", 0)
+                
+            blueprint_grp = cmds.group(empty=True, name=self.module_namespace+":blueprint_joints_grp")
+            cmds.parent(new_joints[0], blueprint_grp, absolute=True)
+            
+            creation_pose_grp_nodes = cmds.duplicate(blueprint_grp, name=self.module_namespace+":creationPose_joints_grp", renameChildren=True)
+            creation_pose_grp = creation_pose_grp_nodes[0]
+            
+            creation_pose_grp_nodes.pop(0)
+            i = 0
+            for node in creation_pose_grp_nodes:
+                renamed_node = cmds.rename(node, self.module_namespace+":creationPose_" + self.joint_info[i][0])
+                cmds.setAttr(renamed_node+".visibility", 0)
+                
+            
+                
+                    
+                
